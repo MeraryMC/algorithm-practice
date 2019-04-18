28385326bfd4f28991f88e744ca9264240960c06
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-04-17 19:47:11 -0700

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //make result array
+  var result = [];
+  var inner = [];
 
+  //account for chunk being greater than the array length
+  if (size > array.length){
+    return array;
+  } else {
+    while (array.length > 0){ //important to keep track of because we're going to keep cutting down the array
+    inner = array.slice(0, size);
+    // console.log(inner);
+    result.push(inner);
+    array = array.splice(size);
+    }
+    return result;
+  }
+}
+
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
 module.exports = chunk;
