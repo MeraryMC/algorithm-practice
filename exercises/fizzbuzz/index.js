d5a7c560876717f64a8ee09edf87719d8bd7ebb6
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-04-17 17:22:12 -0700

    maxchar and fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,40 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  //iterate through the numbers between 1 and n, inclusive
+  for (let i = 1; i < n + 1; i++){
+    //if the number is a multiple of 3 but not 5
+    if (i % 3 === 0 && i % 5 !== 0){
+      //print fizz
+      console.log('fizz');
+      //skip to the next i
+      continue;
+    }
+    //if the number is a mulitple of 5 but not 3
+    if (i % 3 !== 0 && i % 5 === 0){
+      //print buzz
+      console.log('buzz');
+      //skip to the next i
+      continue;
+    }
+    //if the number is a multiple of both 3 and 5
+    if (i % 3 === 0 && i % 5 === 0){
+      //print fizzbuzz
+      console.log('fizzbuzz');
+      //skip to the next i
+      continue;
+    }
+    else {
+      //if none of those conditions are met, print the number
+      console.log(i);
+    }
+  }
+}
+
+//Notes: the continue statements are necessary here so that we continue to print without
+//breaking, AND by replacing the printing of the numbers
+
+console.log(fizzBuzz(200));
 
 module.exports = fizzBuzz;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,33 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  //make empty object
+  let charObj = {};
+  //helper variable
+  let max = 0;
+  //variable to hold the max character
+  let maxChar = '';
+  for (let char of str){ //used to iterate through a string, array, or iritable object
+    //let the property of char at charObj equal its own value plus one, unless the value is falsy (which means that charObj[char] was undefined), in which case assign the value of 1
+    charObj[char] = charObj[char] + 1 || 1;
+  }
+  console.log(charObj);
+
+  for (let char in charObj){ //used to iterate through an obejct with key value pairs
+      if (charObj[char] > max){
+        max = charObj[char];
+        maxChar = char;
+      }
+    }
+  return maxChar;
+}
+
+
+
+console.log(maxChar("apple 1231111"));
+
 
 module.exports = maxChar;
+
+
